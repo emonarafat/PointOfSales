d79a66036ed3d659bd31274ef3cd23cdf9a727df
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-02 00:24:46 +0300

    Available sales combinations feature

---
+++ PointOfSales.Specs/AvailableSalesCombinations.feature
@@ -0,0 +1,9 @@
+﻿Feature: AvailableSalesCombinations
+	In order to suggest a discount
+	As a salesman
+	I want to see the available sales combinations when selecting a product
+
+Scenario: No sales combinations available
+	Given I have selected product without sales combinaions	
+	When I am trying to see available sales combinations
+	Then I do not see any available sales combinations
---
+++ PointOfSales.Specs/Steps/SalesCombinationSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace PointOfSales.Specs.Steps
+{
+    [Binding]
+    public class SalesCombinationSteps
+    {
+        [Given(@"I have selected product without sales combinaions")]
+        public void GivenIHaveSelectedProductWithoutSalesCombinaions()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I am trying to see available sales combinations")]
+        public void WhenIAmTryingToSeeAvailableSalesCombinations()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I do not see any available sales combinations")]
+        public void ThenIDoNotSeeAnyAvailableSalesCombinations()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
