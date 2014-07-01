26b2eadbc211d08db83abbd7d349583178dabd6c
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-01 18:19:52 +0300

    Product repository added

---
+++ PointOfSales.Persistence/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using PointOfSales.Domain.Model;
+using PointOfSales.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSales.Persistence
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetByNameOrDescription(string search)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
