7b198db7805b73e9451495073c90084483b1fe06
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-01 19:14:08 +0300

    Products browsing feature added

---
+++ PointOfSales.Specs/ProductsBrowsing.feature
@@ -0,0 +1,10 @@
+﻿Feature: ProductsBrowsing
+	In order to manage products
+	As a salesman
+	I want to see all available products
+
+@products
+Scenario: No products available
+	Given I have no products	
+	When I trying to see all available products
+	Then I do not see any products
---
+++ PointOfSales.Specs/Steps/ProductSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace PointOfSales.Specs.Steps
+{
+    [Binding]
+    public class ProductSteps
+    {
+        private static readonly string connectionString = "server=(localdb)\\v11.0;database=PoS;Integrated Security=SSPI";
+
+        [Given(@"I have no products")]
+        public void GivenIHaveNoProducts()
+        {
+            string sql = @"
+IF EXISTS(SELECT 1 FROM sys.tables WHERE object_id = OBJECT_ID('Products'))
+BEGIN;
+    DROP TABLE Products;
+END;
+
+CREATE TABLE Products (
+    ProductID INTEGER NOT NULL IDENTITY(1, 1),
+    Name VARCHAR(255) NOT NULL,
+    Price DECIMAL(18,2) NOT NULL,
+    Description VARCHAR(MAX) NOT NULL,
+    PictureURL VARCHAR(255) NULL,
+    EntryDate DATETIME NOT NULL,
+    PRIMARY KEY (ProductID)
+);
+";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            using(SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        [When(@"I trying to see all available products")]
+        public void WhenITryingToSeeAllAvailableProducts()
+        {
+            
+        }
+
+        [Then(@"I do not see any products")]
+        public void ThenIDoNotSeeAnyProducts()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
