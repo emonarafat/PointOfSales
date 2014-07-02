eee8599d71df57bdc081d1abb2c072368c819b40
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-02 20:13:56 +0300

    Logging for sales combinations repository

--- PointOfSales.Persistence/SalesCombinationRepository.cs
+++ PointOfSales.Persistence/SalesCombinationRepository.cs
@@ -7,20 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using NLog;
 
 namespace PointOfSales.Persistence
 {
     public class SalesCombinationRepository : Repository, ISalesCombinationRepository
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public IEnumerable<SalesCombination> GetByProductId(int productId)
         {
+            Logger.Debug("Getting sales combinations for product {0}", productId);
             var sql = @"SELECT * FROM SalesCombinations 
                         WHERE MainProductID = @productId OR SubProductID = @productId";
 
             using (var conn = GetConnection())
-            {                
                 return conn.Query<SalesCombination>(sql, new { productId });
-            }  
         }
     }
 }
