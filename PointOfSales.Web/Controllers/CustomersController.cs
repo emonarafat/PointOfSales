3f4d57226a2be6c4949e25a239fcd3f380ba30e9
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-11 19:20:16 +0300

    Customers controller added

---
+++ PointOfSales.Web/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using PointOfSales.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PointOfSales.Web.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        public IEnumerable<Customer> Get()
+        {
+            return Enumerable.Empty<Customer>();
+        }
+
+        public void Post(Customer customer)
+        {
+
+        }
+    }
+}
