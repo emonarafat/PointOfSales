4b4149c6545aef647fef395406dee272a2b4c827
lazyberezovsky <sergey.berezovskiy@gmail.com>
2014-07-11 19:08:17 +0300

    Adding customers

---
+++ PointOfSales.Specs/AddingCustomer.feature
@@ -0,0 +1,9 @@
+﻿Feature: AddingCustomer
+	In order suggest discounts and spam
+	As a salesman
+	I want to be able to enter the details of a customer in the system
+
+Scenario: Adding new customer
+	Given I don't have any customers	
+	When I add customer
+	Then customer should exist in the system
---
+++ PointOfSales.Specs/Steps/CustomerSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace PointOfSales.Specs.Steps
+{
+    [Binding]
+    public class CustomerSteps
+    {
+        [Given(@"I don't have any customers")]
+        public void GivenIDonTHaveAnyCustomers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I add customer")]
+        public void WhenIAddCustomer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"customer should exist in the system")]
+        public void ThenCustomerShouldExistInTheSystem()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
